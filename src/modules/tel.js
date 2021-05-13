0ca93566767d64f9c671e4f14bbe2bb347f7fa77
Oksana Ovchinnikova <oksana_ovchinnikova@inbox.ru>
2021-05-13 18:34:59 +0500

    create toggle Repair types

--- src/modules/tel.js
+++ src/modules/tel.js
@@ -80,6 +80,9 @@ const toggleMenu = () => {
       if (target.closest(".popup-menu-nav__item")) {
         closeMenu();
       }
+      if (target.closest(".popup-menu") && target.closest(".link-list")) {
+        closeMenu();
+      }
     });
     buttonOpen.addEventListener("click", openMenu);
   }
@@ -102,3 +105,29 @@ const scrollLigth = () => {
   });
 };
 scrollLigth();
+
+const toggleRepairTypes = () => {
+  const popupRepairTypes = document.querySelector(".popup-repair-types");
+  const btnClose = popupRepairTypes.querySelectorAll(".close.mobile-hide");
+
+  const openRepairTypes = () => {
+    popupRepairTypes.style.visibility = "visible";
+  };
+  document.addEventListener("click", (event) => {
+    const target = event.target;
+    if (target.closest(".link-list")) {
+      openRepairTypes();
+    }
+    if (target.closest(".popup-repair-types") && target.closest(".close")) {
+      popupRepairTypes.style.visibility = "hidden";
+    }
+  });
+  btnClose.forEach((item) => {
+    item.addEventListener("click", () => {
+      popupRepairTypes.style.visibility = "hidden";
+    });
+  });
+};
+toggleRepairTypes();
+
+regNumber = /[^0-9]/gi;
