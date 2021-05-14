75d76b89bbe409ac27f394bd6a5b505af4e8441d
Oksana Ovchinnikova <oksana_ovchinnikova@inbox.ru>
2021-05-14 17:40:29 +0500

    fix hint

--- src/modules/hint.js
+++ src/modules/hint.js
@@ -10,7 +10,10 @@ const hint = () => {
       if (target === item) {
         textHint[i].style.visibility = "visible";
         textHint[i].style.opacity = 1;
-        textHint.querySelector("[:before]");
+        if (textHint[i].getBoundingClientRect().top < 0) {
+          textHint[i].classList.add("formula-item-popup__mod");
+          textHint[i].style.bottom = `-${textHint[i].clientHeight + 10}px`;
+        }
       }
     });
   };
@@ -21,6 +24,13 @@ const hint = () => {
       if (target === item) {
         textHint[i].style.visibility = "hidden";
         textHint[i].style.opacity = 0.1;
+        textHint[i].classList.remove("formula-item-popup__mod");
+        if (textHint[i].getBoundingClientRect().top < 0) {
+          textHint[i].classList.remove("formula-item-popup__mod");
+          textHint[i].style.bottom = "90px";
+        } else {
+          textHint[i].style.bottom = "90px";
+        }
       }
     });
   };
