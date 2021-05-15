092c1934ebf233a7766c22805de5b718fc4d09b0
Oksana Ovchinnikova <oksana_ovchinnikova@inbox.ru>
2021-05-16 00:23:45 +0500

    create mask for phone

--- src/modules/sendForms.js
+++ src/modules/sendForms.js
@@ -96,10 +96,48 @@ const sendForms = () => {
   btnCloseSuccess.addEventListener("click", () => {
     successMsg.style.visibility = "hidden";
   });
-  regularPhone = () => {
+  var keyCode;
+  function mask(event) {
+    event.keyCode && (keyCode = event.keyCode);
+    var pos = this.selectionStart;
+    if (pos < 3) event.preventDefault();
+    var matrix = "+7 (___) ___-__-__",
+      i = 0,
+      def = matrix.replace(/\D/g, ""),
+      val = this.value.replace(/\D/g, ""),
+      new_value = matrix.replace(/[_\d]/g, function (a) {
+        return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+      });
+    i = new_value.indexOf("_");
+    if (i != -1) {
+      i < 5 && (i = 3);
+      new_value = new_value.slice(0, i);
+    }
+    var reg = matrix
+      .substr(0, this.value.length)
+      .replace(/_+/g, function (a) {
+        return "\\d{1," + a.length + "}";
+      })
+      .replace(/[+()]/g, "\\$&");
+    reg = new RegExp("^" + reg + "$");
+    if (
+      !reg.test(this.value) ||
+      this.value.length < 5 ||
+      (keyCode > 47 && keyCode < 58)
+    )
+      this.value = new_value;
+    if (event.type == "blur" && this.value.length < 5) this.value = "";
+  }
+  const regularPhone = () => {
     const inputPhone = document.querySelectorAll(`input[name="phone"]`);
-    const mask = inputPhone.forEach((phone) => {});
+
+    inputPhone.forEach((phone) => {
+      phone.addEventListener("input", mask, false);
+      phone.addEventListener("blur", mask, false);
+      phone.addEventListener("focus", mask, false);
+    });
   };
+  regularPhone();
 };
 
 export default sendForms;
