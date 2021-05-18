b316259cc2e262f5ccc06183829f7bb9c383c515
Oksana-png <73472712+Oksana-png@users.noreply.github.com>
2021-05-18 21:11:05 +0500

    fix transparency

--- src/modules/transparency.js
+++ src/modules/transparency.js
@@ -35,40 +35,38 @@ const transparency = () => {
   });
 
   const sliderModal = () => {
-    if (window.innerWidth <= 1091) {
-      const maxSlide = itemModalSlide.length;
-      const runSlide = () => {
-        if (currentSlide < 0) {
-          currentSlide = maxSlide - 1;
-        }
-        if (currentSlide > maxSlide - 1) {
-          currentSlide = 0;
-        }
-        nextSlide(itemModalSlide, currentSlide);
-      };
-      const nextSlide = (elem, index) => {
-        elem[index].style.display = "block";
-        totalSlide.textContent = itemModalSlide.length;
-        numderSlide.textContent = currentSlide + 1;
-      };
-      const prevSlide = (elem, index) => {
-        elem[index].style.display = "none";
-        totalSlide.textContent = itemModalSlide.length;
-        numderSlide.textContent = currentSlide + 1;
-      };
-      document.addEventListener("click", (e) => {
-        const target = e.target;
-        if (target.closest("#transparency_right")) {
-          prevSlide(itemModalSlide, currentSlide);
-          currentSlide++;
-          runSlide();
-        } else if (target.closest("#transparency_left")) {
-          prevSlide(itemModalSlide, currentSlide);
-          currentSlide--;
-          runSlide();
-        }
-      });
-    }
+    const maxSlide = itemModalSlide.length;
+    const runSlide = () => {
+      if (currentSlide < 0) {
+        currentSlide = maxSlide - 1;
+      }
+      if (currentSlide > maxSlide - 1) {
+        currentSlide = 0;
+      }
+      nextSlide(itemModalSlide, currentSlide);
+    };
+    const nextSlide = (elem, index) => {
+      elem[index].style.display = "block";
+      totalSlide.textContent = itemModalSlide.length;
+      numderSlide.textContent = currentSlide + 1;
+    };
+    const prevSlide = (elem, index) => {
+      elem[index].style.display = "none";
+      totalSlide.textContent = itemModalSlide.length;
+      numderSlide.textContent = currentSlide + 1;
+    };
+    document.addEventListener("click", (e) => {
+      const target = e.target;
+      if (target.closest("#transparency_right")) {
+        prevSlide(itemModalSlide, currentSlide);
+        currentSlide++;
+        runSlide();
+      } else if (target.closest("#transparency_left")) {
+        prevSlide(itemModalSlide, currentSlide);
+        currentSlide--;
+        runSlide();
+      }
+    });
   };
   sliderModal();
 
